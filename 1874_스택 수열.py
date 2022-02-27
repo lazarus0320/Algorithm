a64c88cb9e317b304a6e891d0675623cfc2c5722
lazarus0320 <astar5327z@gmail.com>
2022-02-27 11:35:17 +0900

    22-02-26

--- 1874_스택 수열.py
+++ 1874_스택 수열.py
@@ -2,15 +2,24 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 n = int(input())
-lst = [int(input()) for _ in range(n)]
-slst =[]
 stack = deque()
-j = 0
-cnt = 0
-for i in range(1, n+1):
-    stack.append('+')
-    slst.append(i)
-    cnt += 1
-    if slst[-1] == lst[j]:
+lst = []
+cnt = 1
+flag = True
+for _ in range(n):
+    num = int(input())
+    while cnt <= num:
+        lst.append(cnt)
+        stack.append('+')
+        cnt += 1
+    if num == lst[-1]:
+        lst.pop()
         stack.append('-')
-        stack.pop()
+    else:
+        flag = False
+
+if flag == False:
+    print('NO')
+else:
+    for i in stack:
+        print(i)
