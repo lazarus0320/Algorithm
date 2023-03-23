f91d0b1af826db3b6457c9076a4e7db911f2b321
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-03-23 23:39:11 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 328 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N, T, max, min, temp;
+  cin >> T;
+  for (int i = 0; i < T; i++) {
+    
+    max = -1000000;
+    min = 1000000;
+    cin >> N;
+
+    for (int j = 0; j < N; j++) {
+      cin >> temp;
+      if (max < temp) max = temp;
+      if (min > temp) min = temp;
+    }
+    cout << min << " " << max << "\n";
+  }
+
+}
