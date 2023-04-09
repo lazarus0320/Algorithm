5f408ba470b85bc1ebe317e404897fbad48cba7d
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-04-09 17:41:09 +0900

    [Silver III] Title: 주유소, Time: 16 ms, Memory: 3456 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -2,34 +2,28 @@
 using namespace std;
 typedef long long ll;
 int main() {
-  /**
-   * 비싼 곳에서는 다음 도시까지의 거리만큼만 사고,
-   * 제일 싼 곳에서 존나 다 산다.
-   * 
-   * 최소값의 도시에서는 남아있는 거리만큼 다 산다.
-   * 
-   */
-  int cityNum;
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ll cityNum;
   cin >> cityNum;
-  int loadLength[cityNum-1];
-  int cityPrice[cityNum];
-  ll totalLoad = 0;
-  for (int i = 0; i < cityNum-1; i++) {
+  ll loadLength[cityNum-1];
+  ll cityPrice[cityNum];
+  for (ll i = 0; i < cityNum-1; i++) {
     cin >> loadLength[i];
-    totalLoad += loadLength[i];
   }
   
-  for (int i = 0; i < cityNum; i++) {
+  for (ll i = 0; i < cityNum; i++) {
     cin >> cityPrice[i];
   }
-  int minPrice = 10001;
+  ll minPrice = 10001;
 
   ll ans = 0;
-  for (int i = 0; i < cityNum-1; i++) {
+  for (ll i = 0; i < cityNum-1; i++) {
     minPrice = min(minPrice, cityPrice[i]);
     ans += minPrice * loadLength[i];
   }
-  cout << ans; 
+  cout << ans;
 
   return 0;
 }
