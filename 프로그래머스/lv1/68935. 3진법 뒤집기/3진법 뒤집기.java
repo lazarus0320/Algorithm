f022c4955b757abb75ccb29b95fe2fbfbccab965
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-05-22 18:11:32 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        while(n > 0) {
+            sb.append(n % 3);
+            n /= 3;
+        }
+        answer = Integer.parseInt(sb.toString(), 3);
+        return answer;
+    }
+}
