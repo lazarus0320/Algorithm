8c08c09b767c0588f2f4f0e3d0d196d284e53890
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-05-24 21:06:02 +0900

    [level 1] Title: 예산, Time: 0.52 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -1,15 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        Arrays.sort(d);
-        int cnt = 0;
-        for (int i = 0; i < d.length; i++) {
-            if (d[i] + answer <= budget) {
-                answer += d[i];
-                cnt++;
-            }
-        }
-        return cnt;
+        return answer;
     }
 }
