499b74f874ecd6b1438c4419a7b3b8e492dc6b25
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-07-23 10:18:44 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] weekDay = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+        int day = 0;
+        for (int i = 0; i < a - 1; i++) {
+            day += month[i];
+        }
+        day += b;
+        answer = weekDay[day % 7];
+        return answer;
+    }
+}
