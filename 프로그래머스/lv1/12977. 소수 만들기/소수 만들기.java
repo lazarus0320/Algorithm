f4d6fbca10beb339408681f64525ef6321146255
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-07-26 10:02:16 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j <nums.length; j++) {
+                for (int k = j+1; k <nums.length; k++) {
+                    if (isPrime(nums[i] + nums[j] + nums[k])) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
