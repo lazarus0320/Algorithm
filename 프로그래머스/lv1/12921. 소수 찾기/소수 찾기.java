89a048e598d448d47ce453b7cd9aa3dd8acfc7a6
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-07-28 09:19:42 +0900

    [level 1] Title: 소수 찾기, Time: 190.86 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) answer++;
+        }
+        return answer;
+    }
+}
