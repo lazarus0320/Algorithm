d812a40d697a414f4079e75d1ff3ea7515e75709
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-08-02 13:57:51 +0900

    [unrated] Title: 옹알이 (2), Time: 0.79 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,13 +3,6 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         String[] words = {"aya", "ye", "woo", "ma"};
-        /*
-        일단 babbling을 순회하면서 문자열 하나씩 대본다
-        words중에 bab에 있는 값들중 contains true가 있는가?
-        있으면 해당 bab에서 words의 요소를 잘라냄
-        연음 발음 안되므로 해당 key값에 대해 카운트를 올려야함.
-        
-        */
         
         for (String bab: babbling) {
             if (bab.contains("ayaaya") || bab.contains("yeye") || 
@@ -20,8 +13,6 @@ class Solution {
                 bab = bab.replace(word, " ");
             }
             bab = bab.replace(" ", "");
-            //yyemaewoo -> ye -> 발음가능 처리가 되어 버림..
-            //-> 'y e ' -> 
             if (bab.equals("")) {
                 answer++;
             }
