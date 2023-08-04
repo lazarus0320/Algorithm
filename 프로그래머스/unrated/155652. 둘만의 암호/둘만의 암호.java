bd669fde166faa91e93d038799194160a38d53f7
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-08-04 11:26:24 +0900

    [unrated] Title: 둘만의 암호, Time: 0.92 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,24 +1,24 @@
 class Solution {
     public String solution(String str, String skip, int index) {
         StringBuilder sb = new StringBuilder();
-        int[] skipArr = new int[26];
-        
-        for (char c : skip.toCharArray()) { //skip 단어들을 1로 설정
-            skipArr[(int)c - 97]++;
-        }
-        
+
         for (char c : str.toCharArray()) {
             int cnt = 0;
-            int num = (int)c;
+            char nextChar = c;
+
+            // Find the next valid character by incrementing according to index
             while (cnt < index) {
-                num = (num + 1 - 'a') % 26 + 'a'; // Adjust the range to 'a' to 'z'
-                if (skipArr[num - 'a'] == 1) {
-                    continue;
-                } else {
+                nextChar = (char) (nextChar + 1);
+                if (nextChar > 'z') {
+                    nextChar = 'a'; // Wrap around to 'a' if we reach 'z'
+                }
+
+                if (!skip.contains(String.valueOf(nextChar))) {
                     cnt++;
                 }
             }
-            sb.append( Character.toString((char)num) );
+
+            sb.append(nextChar);
         }
         return sb.toString();
     }
