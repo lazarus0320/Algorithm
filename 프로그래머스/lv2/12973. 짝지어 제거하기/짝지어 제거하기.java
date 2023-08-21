a5bf89674917b44118a968363f52faaa7f1a4f94
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-08-21 10:06:38 +0900

    [level 2] Title: 짝지어 제거하기, Time: 52.05 ms, Memory: 60.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            }
+            else if (c == stack.peek()) {
+                stack.pop();
+            }
+            else {
+                stack.push(c);
+            }
+        }
+        
+        if (stack.isEmpty()) {
+            answer = 1;
+            return answer;
+        }
+        return answer;
+    }
+}
