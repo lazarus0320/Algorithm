4741a756b6081fcdd95f33a54aaa4f8b000f2f0d
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-09-16 12:57:03 +0900

    [level 2] Title: 프로세스, Time: 0.59 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.java
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -1,45 +1,25 @@
 import java.util.*;
-
-class Position {
-    int priority;
-    int idx;
-
-    Position(int priority, int idx) {
-        this.priority = priority;
-        this.idx = idx;
-    }
-}
-
 class Solution {
     public int solution(int[] priorities, int location) {
-        
         int answer = 0;
-        Queue<Position> q = new LinkedList<>();
-        int[] temp = Arrays.copyOf(priorities, priorities.length);
-        Integer[] priArr = Arrays.stream(temp).boxed().toArray(Integer[]::new);
-        Arrays.sort(priArr, Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        for (int i = 0; i < priorities.length; i++) {
-            q.offer(new Position(priorities[i], i));
+        for (int p : priorities) {
+            pq.add(p);
         }
         
-        int idx = 0;
-        while (!q.isEmpty()) {
-            Position pos = q.poll();
-            
-            if (pos.priority == priArr[idx]) {
-                answer++;
-                idx++;
-                
-                if (pos.idx == location) {
-                    break;
-                }
-            }
-            else {
-                q.offer(pos);
+        while(!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    pq.poll();
+                    answer++;
+                    
+                    if (i == location) {
+                        return answer;
+                    }
+                } 
             }
         }
         return answer;
-
     }
 }
