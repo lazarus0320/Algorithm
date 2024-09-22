3cfdb946b091b61a3c1b30281ed80e9add82d4e2
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2024-09-22 09:54:34 +0900

    [level 1] Title: 옹알이 (2), Time: 1.32 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,35 +1,23 @@
 class Solution {
     public int solution(String[] babbling) {
-        String[] canSpeakArr = {"aya", "ye", "woo", "ma"};
         int answer = 0;
-
+        String[] canSpeakArr = {"aya", "ye", "woo", "ma"};
+        
         for (String bab: babbling) {
-            String prev = "";
-            boolean isValid = true;
-
-            while (!bab.isEmpty()) {
-                boolean found = false;
-
-                for (String now : canSpeakArr) {
-                    if (bab.startsWith(now) && !now.equals(prev)) {  // 발음이 앞부분에 있고, 연속된 발음이 아니면
-                        bab = bab.substring(now.length());
-                        prev = now;
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) {  // 유효한 발음을 찾지 못하면
-                    isValid = false;
-                    break;
-                }
+            if (bab.contains("ayaaya") || bab.contains("yeye")
+               || bab.contains("woowoo") || bab.contains("mama")) {
+                continue;
             }
-
-            if (isValid) {
+            
+            for (String word: canSpeakArr) {
+                bab = bab.replace(word, " ");
+            }
+            bab = bab.replace(" ", "");
+            
+            if (bab.equals("")) {
                 answer++;
             }
         }
-
         return answer;
     }
 }
