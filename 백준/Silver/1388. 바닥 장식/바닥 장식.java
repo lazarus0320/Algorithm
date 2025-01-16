fb8bde3822a102f18ceeeab5dcc5f7b7dc0f0a9e
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2025-01-16 14:06:44 +0900

    [Silver IV] Title: 바닥 장식, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -1,19 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
-
-class Node {
-	final int cal;
-	final int row;
-
-	public Node(int cal, int row) {
-		this.cal = cal;
-		this.row = row;
-	}
-}
 
 class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,52 +9,21 @@ class Main {
 	static int M = 0;
 	static char[][] field;
 	static boolean[][] visit;
-	static Queue<Node> queue = new LinkedList<>();
-	static int[] horizonDx = {-1, 1};
-	static int[] verticalDy = {1, -1};
 
-	static int bfs(int cal, int row) {
-		int count = 0;
+	static void dfs(int cal, int row, boolean isRow) {
 		visit[cal][row] = true;
-		int startChar = field[cal][row];
-		count++;
-		Node node = new Node(cal, row);
-		queue.add(node);
-
-		while (!queue.isEmpty()) {
-			Node curNode = queue.poll();
-
-			if (field[curNode.cal][curNode.row] == startChar) {
-				if (field[curNode.cal][curNode.row] == '-') {
-					for (int i = 0; i < horizonDx.length; i++) {
-						int dx = curNode.row + horizonDx[i];
-						if (dx >= 0 && dx < M
-							&& field[curNode.cal][dx] == '-' && !visit[curNode.cal][dx]) {
-							visit[curNode.cal][dx] = true;
-							Node nextNode = new Node(curNode.cal, dx);
-							queue.add(nextNode);
-						}
-					}
-				}
-				else {
-					for (int i = 0; i < verticalDy.length; i++) {
-						int dy = curNode.cal + verticalDy[i];
-						if (dy >= 0 && dy < N
-							&& field[dy][curNode.row] == '|' && !visit[dy][curNode.row]) {
-							visit[dy][curNode.row] = true;
-							Node nextNode = new Node(dy, curNode.row);
-							queue.add(nextNode);
-						}
-					}
-				}
-			}
-
-			else {
-				break;
+		if (isRow) {
+			row++;
+			if (row < M && field[cal][row] == '-') {
+				dfs(cal, row, true);
 			}
 		}
-		queue.clear();
-		return count;
+		else {
+			cal++;
+			if (cal < N && field[cal][row] == '|') {
+				dfs(cal, row, false);
+			}
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -84,9 +41,16 @@ class Main {
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (!visit[i][j]) {
-					answer += bfs(i, j);
+				if (visit[i][j]) {
+					continue;
 				}
+				if (field[i][j] == '-') {
+					dfs(i, j, true);
+				}
+				else {
+					dfs(i, j, false);
+				}
+				answer++;
 			}
 		}
 
