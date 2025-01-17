4207493ea387be9107e96cc911ff1cadd1845f13
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2025-01-17 12:02:27 +0900

    [Silver I] Title: 단지번호붙이기, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,86 +1,62 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
-    static class Point { // 큐에서 x, y 좌표 뽑아내기용 객체
-        int x;
-        int y;
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N = 0;
+	static int M = 0;
+	static int[] dRow = {0, 0, -1, 1};
+	static int[] dCol = {-1, 1, 0, 0};
+	static int[][] field;
+	static boolean[][] visit;
 
-    static int N, danjiNum = 0;
-    static int dx[] = { -1, 1, 0, 0 };
-    static int dy[] = { 0, 0, -1, 1 };
-    static int [] danji;
-    static int [][] map;
-    static boolean [][] visited;
+	static int dfs(int col, int row, int sum) {
+		visit[col][row] = true;
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
-        danji = new int[N * N]; // NXN 크기 맵에서 단지 최대 개수는 N*N
+		for (int i = 0; i < 4; i++) {
+			int newCol = col + dCol[i];
+			int newRow = row + dRow[i];
 
+			if (newCol >= 0 && newCol < N && newRow >= 0 && newRow < N) {
+				if (!visit[newCol][newRow] && field[newCol][newRow] == 1) {
+					sum = dfs(newCol, newRow, sum + 1);
+				}
+			}
+		}
+		return sum;
+	}
 
-        StringTokenizer st;
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String temp = st.nextToken(); // 각 행마다 하나의 문자열 입력 받음
-            for (int j = 0; j < N; j++) {
-                map[i][j] = temp.charAt(j) - '0'; // 문자를 정수로 치환하여 각 열에 할당
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		List<Integer> danjiList = new ArrayList<>();
+		field = new int[N][N];
+		visit = new boolean[N][N];
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    // 집이 있는데 방문이 안된 곳이 있으면 단지수를 추가하고 해당 단지의 집이 몇개 있는지 bfs로 계산하기
-                    danjiNum++;
-                    countingHouseBFS(i, j);
-                }
-            }
-        }
+		for (int i = 0; i < N; i++) {
+			String inputLine = br.readLine();
+			for (int j = 0; j < N; j++) {
+				field[i][j] = inputLine.charAt(j) - '0';
+			}
+		}
 
-        Arrays.sort(danji);
-        System.out.println(danjiNum);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (!visit[i][j] && field[i][j] == 1) {
+					danjiList.add(dfs(i, j, 1));
+				}
+			}
+		}
 
-        for (int i = 1; i < danji.length; i++) {
-            if (danji[i] != 0) System.out.println(danji[i]);
-        }
-
-
-    }
-    public static void countingHouseBFS(int i, int j) {
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(i, j)); // 큐에 새로운 단지의 시작 주소를 좌표값으로 집어 넣음
-        visited[i][j] = true;
-        danji[danjiNum]++; // 해당 단지 번호의 집 개수를 하나 올림
-
-        while(!q.isEmpty()) {
-            // 큐에서 값 하나 뽑기
-            Point temp = q.poll();
-            // 상하좌우를 돌아다니면서 탐색하기
-            for (int k = 0; k < 4; k++) {
-                int x = temp.x + dx[k];
-                int y = temp.y + dy[k];
-                if (x >= 0 && y >= 0 && x < N && y < N) {
-                    if (map[x][y] == 1 && !visited[x][y]) {
-                        // 움직인 위치가 map 범위 내에 있고, 집이 있고, 방문처리가 되지 않았을 경우
-                        // 큐에 좌표를 넣고 방문처리 하고 해당 단지번호의 집 개수를 하나 올림
-                        q.add(new Point(x, y));
-                        visited[x][y] = true;
-                        danji[danjiNum]++;
-                    }
-                }
-            }
-        }
-    }
+		Collections.sort(danjiList);
+		System.out.println(danjiList.size());
+		for (int num : danjiList) {
+			System.out.println(num);
+		}
+	}
 }
