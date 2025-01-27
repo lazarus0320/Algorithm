4a90ee68c69595b7829cf779b2a1a3beed78daa1
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2025-01-27 11:02:35 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 808 ms, Memory: 39392 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			int[] arrA = new int[N];
+			int[] arrB = new int[M];
+
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				arrA[j] = Integer.parseInt(st.nextToken());
+			}
+
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arrB[j] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(arrA);
+			Arrays.sort(arrB);
+			System.out.println(solution(N, M, arrA, arrB));
+		}
+
+	}
+
+	static int solution(int N, int M, int[] arrA, int[] arrB) {
+		int ans = 0;
+
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				if (arrB[i] < arrA[j]) {
+					ans += N - j;
+					break;
+				}
+			}
+		}
+
+		return ans;
+	}
+}
