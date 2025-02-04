ac2f238ab7e7fddd98493ab85ab8205ff6d2c813
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2025-02-04 16:50:40 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 18300 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int dp[] = new int[1000001];
+
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 3;
+
+		for (int i = 4; i <= N; i++) {
+			dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+		}
+
+		System.out.print(dp[N]);
+	}
+}
