7ff118a0f310a4f8627438628a380b0a8cf7775d
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2025-02-04 20:10:28 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,35 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+class Main {
 	public static void main(String[] args) throws IOException {
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		
-		for (int test = 1; test <= T; test++) {
-			
-			int N = Integer.parseInt(br.readLine());
-			
-			int[] dp = new int[N+3];
-			dp[1] = 1;
-			dp[2] = 2;
-			dp[3] = 4;
-			
-			for (int i = 4; i <= N; i++) {
-				
-				dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
 
-				
-			}
-			
-			System.out.println(dp[N]);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+
+		int dp[] = new int[11];
+		dp[0] = 1;
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= 10; i++) {
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
 		}
-		
+
+		for (int cycle = 0; cycle < T; cycle++) {
+			int N = Integer.parseInt(br.readLine());
+			sb.append(dp[N]).append("\n");
+		}
+		System.out.print(sb);
 	}
 }
