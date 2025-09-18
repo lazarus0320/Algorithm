2a0eafa18196631155c3d3ace099574ae613c670
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2025-09-18 22:30:17 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -0,0 +1,12 @@
+import math
+
+def solution(n):
+    answer = 0
+    
+    for i in range(1, int(n * (1/2)) + 1):
+        if n % i == 0:
+            answer += i;
+    
+    answer += n;
+
+    return answer;
