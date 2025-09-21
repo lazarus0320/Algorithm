24a3e927051baaaa24a035c15af5d58b00b8d552
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2025-09-21 22:19:52 +0900

    [level 1] Title: 실패율, Time: 31.82 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,27 +1,24 @@
 def solution(N, stages):
     temp = []
     answer = []
-    dict = {}
-    for i in stages:
-        if i > N:
-            continue
-        dict[i] = dict.get(i, 0) + 1
+    user = 0
+    fail_count = [0] * (N + 1)
     
-    user = len(stages)
+    for stage in stages:
+        if stage <= N:
+            fail_count[stage] += 1
+        user += 1
+    
                       
     for i in range(1, N+1):
-        cur = dict.get(i, 0)
-        
+
         if user == 0:
             temp.append((i, 0))
         else:
-            temp.append((i, cur/user))
+            temp.append((i, fail_count[i]/user))
             
-        user -= cur
+        user -= fail_count[i]
     
     temp.sort(key = lambda x: -x[1])
-    
-    for x, y in temp:
-        answer.append(x)
         
-    return answer
+    return [x[0] for x in temp]
