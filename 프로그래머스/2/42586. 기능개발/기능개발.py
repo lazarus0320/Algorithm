34333f1d9c07c37924aeca5a107e7335cc87dfc1
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2025-09-26 23:10:38 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,23 +2,23 @@ import math
 
 def solution(progresses, speeds):
     answer = []
-    list = []
     
-    for i in range(len(progresses)):
-        day = math.ceil((100 - progresses[i]) / speeds[i])
-        list.append(day)
+    # 1. 각 원소별로 남은 시간을 먼저 리스트화 한다.
+    # 2. 지금 위치한 원소의 남은시간 < 다음 원소 남은시간 -> 1건
+    # 3. 지금 위치한 원소 남은시간(위치 고정) > 다음 원소 남은시간
+    # 3-1. 다음 인덱스 넘어가면서 처음 고정한 위치랑 비교.. 반복하면서 카운트 -> n건 반환
+    
+    prev_time = math.ceil((100 - progresses[0]) / speeds[0])
+    count = 1
+    for i in range(1, len(progresses)):
+        cur_time = math.ceil((100 - progresses[i]) / speeds[i])
         
-    num = 1
-    cur = list[0]
-    
-    for i in range(1, len(list)):
-        if list[i] <= cur:
-            num += 1
+        if prev_time < cur_time:
+            answer.append(count)
+            prev_time = cur_time
+            count = 1
+        
         else:
-            answer.append(num)
-            num = 1
-            cur = list[i]
-        
-    answer.append(num)
-    
+            count += 1
+    answer.append(count)
     return answer
