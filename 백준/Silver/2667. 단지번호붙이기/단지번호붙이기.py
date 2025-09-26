10dc50c3bf1e60ee3a9239049adf85c202ce966e
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2025-09-26 21:34:05 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,32 +1,34 @@
 from collections import deque
 
-
 N = int(input())
 grid = []
 for _ in range(N):
     grid.append(list(input().strip()))
 
 visited = [[False] * N for _ in range(N)]
-directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상하좌우
-complexes = []  # 각 단지의 집 개수 저장
+directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+complexes = []
+
 
 def bfs(start_r, start_c):
-    queue = deque([(start_r, start_c)])
     visited[start_r][start_c] = True
+    queue = deque([(start_r, start_c)])
     count = 1
 
     while queue:
-        cur_r, cur_c = queue.popleft()
+        row, col = queue.popleft()
 
         for dr, dc in directions:
-            nr = cur_r + dr
-            nc = cur_c + dc
+            nr, nc = dr+row, dc+col
 
             if 0 <= nr < N and 0 <= nc < N and grid[nr][nc] == '1' and not visited[nr][nc]:
                 visited[nr][nc] = True
                 queue.append((nr, nc))
                 count += 1
     return count
+
+
+
 
 for i in range(N):
     for j in range(N):
