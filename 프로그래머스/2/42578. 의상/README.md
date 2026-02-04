7a6ae7efa76e0cd28578f5041fcc38182a72f63e
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2026-02-04 15:31:05 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 8.94 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 9.13 MB, 시간: 0.03 ms
+메모리: 8.94 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 23일 23:34:56
+2026년 02월 04일 15:31:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,14 @@
 def solution(clothes):
+    # 종류를 key, 그 수를 dict로 저장. 
+    # item()로 순환하면서 각 종류를 곱한 값 - 1(아예 안입은 경우를 제외)
     answer = 1
-    
     dict = {}
+    for name, type_name in clothes:
+        dict[type_name] = dict.get(type_name, 0) + 1
     
-    for clo, type in clothes:
-        dict[type] = dict.get(type, 0) + 1
-        
     for k, v in dict.items():
-        answer *= v+1
-    
+        answer *= v + 1
     
     return answer - 1
+    
+    
