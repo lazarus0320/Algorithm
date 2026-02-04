3af7c4f1e9b9c3c017320cdf794b4c4bc96af84b
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2026-02-04 15:21:53 +0900

    [level 2] Title: 전화번호 목록, Time: 724.07 ms, Memory: 45.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 45.9 MB, 시간: 586.20 ms
+메모리: 45.8 MB, 시간: 724.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 23일 23:04:46
+2026년 02월 04일 15:21:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,21 @@
 def solution(phone_book):
 
+    # dict에 모든 번호를 등록
+    # phone_book에서 하나씩 꺼내서 딕셔너리에 있는 어떤 값과 그 길이 만큼의 앞부분이 같은지 조사
+    
     book = {}
     
-    for pb in phone_book:
-        book[pb] = book.get(pb, 0) + 1
-        
+    for num in phone_book:
+        book[num] = book.get(num, 0) + 1
+    
+    answer = True
+    
     for num in phone_book:
         for i in range(len(num)):
-            n = num[:i+1]
-            if n in book and n != num:
-                return False
-    return True
+            key = num[:i+1]
+            target = book.get(key, 0)
+            if target == 1 and key != num:
+                answer = False
+                break
+    
+    return answer
