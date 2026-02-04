2ba69d1f303e60acd8d4a8bf8ee0a727418191ab
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2026-02-04 15:35:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 110.74 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,13 +1,11 @@
 def solution(arr):
+    # append할 때 앞 인덱스 값과 다르면 넣기
+    # stack 자체를 답으로?
+    
     stack = []
-    # 1. 순회하면서 넣으려는 값이랑 스택 맨윗값 비교
-    # 2. 다른 거 넣는 거만 넣기
-    
     stack.append(arr[0])
-    
     for i in range(1, len(arr)):
-        if stack[-1] != arr[i]:
+        if arr[i-1] != arr[i]:
             stack.append(arr[i])
-    
     
     return stack
