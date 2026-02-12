a88f4646133e609396add3a60d9caf7e87752808
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2026-02-12 22:33:23 +0900

    [level 2] Title: 가장 큰 수, Time: 74.30 ms, Memory: 20.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,11 @@
 def solution(numbers):
     answer = ''
     
+
     s_numbers = sorted(numbers, key = lambda x: str(x) * 3, reverse = True)
     answer = ''.join(map(str, s_numbers))
     
-    return '0' if answer[0] == '0' else answer
+    if answer[0] == '0':
+        return '0'
+    
+    return answer
