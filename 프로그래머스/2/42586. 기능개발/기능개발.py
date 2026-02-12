709c88de396a80fcfd3752f1bfcd615e96fea2e9
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2026-02-12 18:09:13 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,24 +1,23 @@
 import math
+from collections import deque
 
 def solution(progresses, speeds):
     answer = []
+    days = deque()
     
-    # 1. 각 원소별로 남은 시간을 먼저 리스트화 한다.
-    # 2. 지금 위치한 원소의 남은시간 < 다음 원소 남은시간 -> 1건
-    # 3. 지금 위치한 원소 남은시간(위치 고정) > 다음 원소 남은시간
-    # 3-1. 다음 인덱스 넘어가면서 처음 고정한 위치랑 비교.. 반복하면서 카운트 -> n건 반환
+    for p, s in zip(progresses, speeds):
+        need_days = math.ceil((100 - p) / s)
+        days.append(need_days)
     
-    prev_time = math.ceil((100 - progresses[0]) / speeds[0])
-    count = 1
-    for i in range(1, len(progresses)):
-        cur_time = math.ceil((100 - progresses[i]) / speeds[i])
+    while days:
+        curr_max = days.popleft()
+        count = 1
         
-        if prev_time < cur_time:
-            answer.append(count)
-            prev_time = cur_time
-            count = 1
-        
-        else:
+        while days and days[0] <= curr_max:
+            days.popleft()
             count += 1
-    answer.append(count)
+        
+        answer.append(count)
+            
+    
     return answer
