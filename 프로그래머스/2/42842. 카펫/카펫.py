070f54afb3870f818449ac9f396677f7c3383d2e
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2026-02-13 09:58:53 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -2,10 +2,9 @@ def solution(brown, yellow):
     answer = []
     
     total = brown + yellow
-    for h in range(3, total):
-        
+    
+    for h in range(3, total + 1):
         w = total // h
+        
         if (w - 2) * (h - 2) == yellow:
             return [w, h]
-    
-    return answer
