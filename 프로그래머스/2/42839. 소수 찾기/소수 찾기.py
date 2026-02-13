aa2c0c86541862bd164a779d4ce2d3d5f79a9564
Minjihun <84646738+lazarus0320@users.noreply.github.com>
2026-02-13 09:45:22 +0900

    [level 2] Title: 소수 찾기, Time: 8.27 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -11,10 +11,10 @@ def is_prime(n):
     if n % 2 == 0:
         return False
     
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
+    for i in range(3, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
-    
+        
     return True
 
 def solution(numbers):
@@ -25,11 +25,27 @@ def solution(numbers):
     for length in range(1, len(numbers) + 1):
         for perm in permutations(numbers, length):
             num_str = ''.join(perm)
-            if num_str[0] != '0':
-                all_numbers.add(int(num_str))
+            all_numbers.add(int(num_str))
     
     for num in all_numbers:
         if is_prime(num):
             answer += 1
-            
+    
     return answer
+    
+    
+#     answer = 0
+    
+#     all_numbers = set()
+    
+#     for length in range(1, len(numbers) + 1):
+#         for perm in permutations(numbers, length):
+#             num_str = ''.join(perm)
+#             if num_str[0] != '0':
+#                 all_numbers.add(int(num_str))
+    
+#     for num in all_numbers:
+#         if is_prime(num):
+#             answer += 1
+            
+#     return answer
